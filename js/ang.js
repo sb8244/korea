247e5d3ecec3f9e3c1eaa689f8706fa31449fb8a
Stephen Bussey <sb8244@cs.ship.edu>
2013-03-10 03:55:31 -0400

    fixed weird linking method (why link via js?)

--- js/ang.js
+++ js/ang.js
@@ -3,6 +3,7 @@ angular.module('project', []).
 		$routeProvider.
 		when('/', {controller:MainCtrl, templateUrl:'main.html'}).
 		when('/phrases-main', {controller: PhrasesCtrl, templateUrl:'phrases-main.html'}).
+		when('/phrases-greeting', {controller: PhrasesCtrl, templateUrl:'phrases/greeting.html'}).
 		when('/food-main', {controller: FoodCtrl, templateUrl: 'food-main.html'}).
 		when('/ettiquette-main', {controller: EttCtrl, templateUrl: 'ettiquette-main.html'}).
 		when('/around-main', {controller: AroundCtrl, templateUrl: 'around-main.html'}).
@@ -14,22 +15,6 @@ function MainCtrl($scope, $location) {
 		$scope.show = {};
 	}	
 	$scope.show.up = false;
-	$scope.phrases = function() {
-		$scope.show.up = true;
-		$location.path("/phrases-main");
-	}
-	$scope.food = function() {
-		$scope.show.up = true;
-		$location.path("/food-main");
-	}
-	$scope.ettiquette = function() {
-		$scope.show.up = true;
-		$location.path("/ettiquette-main");
-	}
-	$scope.around = function() {
-		$scope.show.up = true;
-		$location.path("/around-main");
-	}
 	$scope.bulletin = function() {	
 		var url = 'http://sgee.sch.ac.kr/board/list.php?db=asian1';
 		window.plugins.childBrowser.showWebPage(url, {showLocationBar : false}); 
@@ -37,12 +22,32 @@ function MainCtrl($scope, $location) {
 	}
 }
 function PhrasesCtrl($scope, $location) {
+	if($scope.show == undefined)
+	{
+		$scope.show = {};
+	}	
+	$scope.show.up = true;
 }
 function FoodCtrl($scope, $location) {
+	if($scope.show == undefined)
+	{
+		$scope.show = {};
+	}	
+	$scope.show.up = true;
 }
 function AroundCtrl($scope, $location) {
+	if($scope.show == undefined)
+	{
+		$scope.show = {};
+	}	
+	$scope.show.up = true;
 }
 function EttCtrl($scope, $location) {
+	if($scope.show == undefined)
+	{
+		$scope.show = {};
+	}	
+	$scope.show.up = true;
 }
 function BulletinCtrl($scope, $location, $http) {
 	var url = 'http://sgee.sch.ac.kr/board/list.php?db=asian1';
